67c685769baf889222ddf48536f52c6fbe4448b1
nstppbl <1nstppbl@gmail.com>
2022-09-12 14:27:55 +0500

    add try catch blocks

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -9,16 +9,19 @@ import java.util.HashMap;
 
 public class FileReader {
 
-    public Profile getDataFromFile(File file) throws IOException {
+    public Profile getDataFromFile(File file) {
         HashMap <String, String> map = new HashMap<>();
         String input;
 
-        BufferedReader reader = new BufferedReader(new java.io.FileReader(file));
+        try (BufferedReader reader = new BufferedReader(new java.io.FileReader(file))) {
         while ((input = reader.readLine()) != null) {
             String[] keyValuePair = input.split(": ", 2);
             String key = keyValuePair[0];
             String value = keyValuePair[1];
             map.put(key, value);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
         return new Profile(map.get("Name"),
